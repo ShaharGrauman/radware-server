89ab02f8d58aefd94d87ecbc7713e9969c9d286c
saeedHijazi <saeedhijazi1998@gmail.com>
2020-01-23 09:54:32 +0200

    index route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var userController = require('../controllers/users');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  try{
+    const users = await userController.findAll();
+    res.status(200).json(users);
+  }catch(error){
+    res.status(500).json({msg: error.message});
+  }
 });
+
+
 
 module.exports = router;
