69dac50c22cb6eefc210d7898533cbfc5b2bdae3
saeedHijazi <saeedhijazi1998@gmail.com>
2020-01-24 11:07:42 +0200

    Init Server

---
+++ Server/controllers/users.js
@@ -0,0 +1,16 @@
+const User = require("../models/users");
+
+const findAll = async () => {
+    try{
+        const users = await User.getAll();
+        return users;
+    }catch(error){
+        throw new Error(`Cant get users: ${error.message}`);
+    }
+}
+
+
+
+module.exports = {
+    findAll,
+};
---
+++ Server/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var userController = require('../controllers/users');
+
+/* GET users listing. */
+router.get('/', async (req, res) => {
+  try{
+    const users = await userController.findAll();
+    res.status(200).json(users);
+  }catch(error){
+    res.status(500).json({msg: error.message});
+  }
+});
+
+
+
+module.exports = router;
