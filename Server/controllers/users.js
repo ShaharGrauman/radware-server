690080b8cb3abfe0c30ba45cbdd11556430946c0
janatnaa <53605074+janatnaa@users.noreply.github.com>
2020-01-31 13:20:52 +0200

    fixing basic master errors made by some folks

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -17,6 +17,8 @@ const getUserWithRoles = async () => {
             include:{model:roles,attributes:['description'],through: {attributes: []}}
         });
         return data;
+    } catch (error) {
+        throw new Error(`Cant get users with roles: ${error.message}`);
     }
 }
       
