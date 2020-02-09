93495936137a4a71e6143a0633921201c14cfcb3
shahdMansour <shahd_mansour16.com>
2020-02-09 23:45:11 +0200

    resetPassword

--- Server/controllers/sendEmail.js
+++ Server/controllers/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
- sendMail = body => {
+sendMail = body => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -19,6 +19,41 @@ const nodemailer = require('nodemailer');
         subject: 'Radware-SignatureManage', // Subject line
         text: 'Hello world?', // plain text body
         // html: `<h1>Hello ${req.body.name} Node Send Mails</h1>` // html body
+        html: body // html body
+
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log('email sent!!');
+            // res.json({ msg: 'Email has been sent' });
+        }
+    });
+}
+
+
+
+sendEmail = (to, body) => {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'radware.test.mail01@gmail.com', // generated ethereal user
+            pass: 'radware123'  // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: 'radware.test.mail01@gmail.com', // sender address
+        to: to, // list of receivers
+        subject: 'Reset Password', // Subject line
         html: body // html body
 
     };
