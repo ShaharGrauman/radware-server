86d28a3ec6da66396f55a7e644b9f561dba91546
firasGhad <fghadban02@gmail.com>
2020-02-16 20:25:07 +0200

    fix

--- Server/controllers/XML/importXml.js
+++ Server/controllers/XML/importXml.js
@@ -8,20 +8,20 @@ var convert = require('xml-js');
 // With parser
 import_XML_Vuln_Signature = async () => {
   dirname = '../Server/vuln-example-signatures.xml';
-  let colElement= {};
   fs.readFile(dirname, function(err, data) {
-    const result = JSON.parse(convert.xml2json(data, {compact: false, spaces: 4}));
-    console.log(result.elements[1].elements[1].elements[0].elements[0].text);
-    name = result.elements[1].elements[1].name;
-    result.elements[1].elements[1].elements.map((element) => {
-      Object.assign(colElement, {
-        [element.name]: element.elements
+    const signaturesToImport = JSON.parse(convert.xml2json(data, {compact: false, spaces: 4}));
+    signaturesToImport.elements[1].elements.map(signature => {
+      //  console.log(signature);
+       if(signature.name === 'VulnEx'){
+         
+        //  signature.elements.map(element => {
+        //    console.log(element.name);
+        //    console.log(element.elements);
+
+        //  });
+       }
     });
-    console.log(element.elements);
-    });
-    console.log(colElement);
-    // propertyName = result.elements[1].elements[1].elements[0].name;
-    // propertyValue = result.elements[1].elements[1].elements[0].elements[0].text;
+   
     
   });
 }
