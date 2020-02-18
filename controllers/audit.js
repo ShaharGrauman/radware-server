748adefd1e2b934d54c0d6def5ec8a00142feccb
firasGhad <fghadban02@gmail.com>
2020-02-18 16:16:47 +0200

    fixed again

--- controllers/audit.js
+++ controllers/audit.js
@@ -26,6 +26,7 @@ const getData = async (query) => {
             [Op.between]: [startTime, endTime]
         }
     };
+    whereInUsers ={};
     if (query.event != 'all') {
         let event = query.event.split(',');
         Object.assign(where, {
@@ -35,38 +36,33 @@ const getData = async (query) => {
             }
         });
     }
-
-    if (query.user_id != 'all') {
-        let users_id = query.user_id.split(','), id;
-        Object.assign(where, {
-            user_id: {
-                [Op.in]: users_id
+    let users_names;
+    if (query.users_names != 'all') {
+        users_names = query.users_names.split(',');
+        Object.assign(whereInUsers, {
+            username: {
+                [Op.in]: users_names
             }
         });
     }
-
     try {
-        const history = await historyUsersActions.findAll({
-            attributes: ['action_name', 'description', 'time', 'date'],
+        let history = await users.findAll({
+            attributes: ['username'],
             include: [{
-                model: users,
-                attributes: ['username'],
-              }],
-            where: where,
-            order:
+                model: historyUsersActions,
+                attributes: ['action_name', 'description', 'time', 'date'],
+                where: where,
+                order:
                 [
                     ['date', query.orderBy],
                     ['time', query.orderBy]
                 ],
             offset: (parseInt(query.page) - 1) * parseInt(query.size),
             limit: parseInt(query.size),
+              }],
+           where: whereInUsers,
+           
         });
-        history.map(action => {
-            Object.assign(action, { user: action.user.username });
-            return action;
-        //    return {...action, user: action.user.username};
-        });
-        history.map((action) => Object.assign(action, { user: action.user.username }));
         
         let hasNext = true, hasPrev = false;
         if (history.length % (query.size * query.page) != 0 || history.length === 0) {
