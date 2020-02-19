f397b5d7842a7ea351070c2e386923914680aa4b
janatnaa <53605074+janatnaa@users.noreply.github.com>
2020-02-19 12:56:18 +0200

    update constant, severity

--- controllers/signature.js
+++ controllers/signature.js
@@ -24,7 +24,7 @@ const Op = require('Sequelize').Op;
 // const sigByAttack = async () => {
 //     try {
 //         const signaturesByAttack = await signatures.findAll({
-            
+
 //             include:[{model:attack, attributes:['name']}],
 //             group: ['attack_id'],
 //             attributes: ['attack_id', [sequelize.fn('COUNT', 'attack_id'), 'SigCount']],
@@ -37,17 +37,17 @@ const Op = require('Sequelize').Op;
 // }
 const sigBySeverity = async () => {
     try {
-        const signaturesBySeverity = await signatures.findAll({
-            include:[{model:attack, attributes:['name']}],
-            group: ['attack_id','severity'],
-            attributes: ['severity', [sequelize.fn('COUNT', 'severity'), 'attackSev']],
-        }) 
+        const signaturesBySeverity = await attack.findAll({
+            attributes: ['name'],
+            include: [{ model: signatures, attributes: ['severity', [sequelize.fn('COUNT', 'severity'), 'attackSevCount'],] }],
+            group: ['attack_id', 'severity'],
+        })
         console.log(signaturesBySeverity);
         return signaturesBySeverity;
     } catch (error) {
         throw new Error(`Cant get signatures: ${error.message}`);
     }
-} 
+}
 const findAll = async () => {
     try {
         const signatureData = await signatures.findAll();
@@ -219,11 +219,11 @@ const loadSignaturesToExport = async (query) => {
 const sigByAttack = async () => {
     try {
         const signaturesByAttack = await signatures.findAll({
-            
-            include:[{model:attack, attributes:['name']}],
+
+            include: [{ model: attack, attributes: ['name'] }],
             group: ['attack_id'],
             attributes: ['attack_id', [sequelize.fn('COUNT', 'attack_id'), 'SigCount']],
-        }) 
+        })
         console.log(signaturesByAttack);
         return signaturesByAttack;
     } catch (error) {
@@ -236,7 +236,7 @@ const sigPerSeverity = async () => {
         const sigPerSeverity = await signatures.findAll({
             group: ['severity'],
             attributes: ['severity', [sequelize.fn('COUNT', 'severity'), 'SigSevCount']],
-        }) 
+        })
         console.log(sigPerSeverity);
         return sigPerSeverity;
     } catch (error) {
@@ -308,8 +308,8 @@ const create = async (signatureData) => {
     // if (!result) {
     //     return result;
     // }
-    
-    
+
+
 
     signatures.addHook('afterCreate', (signatureDataCreate, options) => {
 
@@ -409,7 +409,7 @@ const create = async (signatureData) => {
             }), date: new Date()
         });
 
-        
+
 
 
         return signatureDataCreate;
@@ -463,7 +463,7 @@ const update = async (DataToUpdate, id) => {
     // if (!result) {
     //     return result;
     // }
-    
+
     try {
         const updatedSignature = signatures.update({
             type: DataToUpdate.type,
