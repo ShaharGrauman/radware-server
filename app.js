fc30cc1b21b3bc2b582382f8996efd85cecb5650
saeedHijazi <saeedhijazi1998@gmail.com>
2020-02-28 14:59:08 +0200

    cors...

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // })
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://radware-signatures.netlify.com");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
