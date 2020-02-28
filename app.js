b1974f0e094898b310a896681beca3b9d3965c97
saeedHijazi <saeedhijazi1998@gmail.com>
2020-02-28 15:07:37 +0200

    cors...

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var loginRouter = require('./routes/login');
 var QaRouter = require('./routes/Qa');
 
 var app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://radware-signatures.netlify.com', credentials: true }));
 // view engine setups
 app.set('views', path.join(__dirname, 'views'));
 
