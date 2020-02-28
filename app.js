828da9377873e7ce521ea8b79af8c30dbb5a488e
saeedHijazi <saeedhijazi1998@gmail.com>
2020-02-28 15:01:36 +0200

    cors...

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, radware"
   );
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
